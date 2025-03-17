19e19c6ca58e6b131c3001bb128d889bc19f658f
Filipe Knoedt <fknoedt@gmail.com>
2025-03-17 17:34:51 -0400

    [071] Time Horizon
    Now it's possible to determine how many days in the future you want to consider the price when buying or selling

---
+++ app/Enum/TimeHorizon.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enum;
+
+use Filament\Support\Contracts\HasLabel;
+
+enum TimeHorizon: string implements HasLabel
+{
+    case ONE = '1';
+    case THREE = '3';
+    case FIVE = '5';
+    case TEN = '10';
+
+    public function getLabel(): ?string
+    {
+        return $this->value;
+    }
+}
