51bdc795b31503ade053103822eef4c6388d106b
Filipe Knoedt <fknoedt@gmail.com>
2025-04-27 02:28:18 -0400

    [105] User Sign Up  * Filament registration() enabled  * /business-card route added  * pre-login page layout improved  * users.role_id default 1  * cleanup

--- resources/views/filament/components/header-left.blade.php
+++ resources/views/filament/components/header-left.blade.php
@@ -1,3 +1,7 @@
-<div class="orbit-btc-logo-wrapper">
-    <img src="{{ asset('images/orbit-btc-large.png') }}" alt="Orbit BTC Logo" class="orbit-btc-logo">
+<div class="orbit-btc-logo-wrapper {{ request()->routeIs('filament.admin.auth.login') ? 'login-page' : 'main-page' }}">
+    <img
+        src="{{ asset(request()->routeIs('filament.admin.auth.login') ? 'images/orbit-btc-large.png' : 'images/orbit-btc-header.png') }}"
+        alt="Orbit BTC Logo"
+        class="orbit-btc-logo"
+    >
 </div>
