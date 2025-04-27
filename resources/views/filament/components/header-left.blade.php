439716d1a17fc02412643b4fcb3af1a9292c9152
Filipe Knoedt <fknoedt@gmail.com>
2025-04-27 02:34:36 -0400

    [105] blade adjustment

--- resources/views/filament/components/header-left.blade.php
+++ resources/views/filament/components/header-left.blade.php
@@ -1,6 +1,6 @@
-<div class="orbit-btc-logo-wrapper {{ request()->routeIs('filament.admin.auth.login') ? 'login-page' : 'main-page' }}">
+<div class="orbit-btc-logo-wrapper {{ request()->routeIs(['filament.admin.auth.login', 'filament.admin.auth.register']) ? 'login-page' : 'main-page' }}">
     <img
-        src="{{ asset(request()->routeIs('filament.admin.auth.login') ? 'images/orbit-btc-large.png' : 'images/orbit-btc-header.png') }}"
+        src="{{ asset(request()->routeIs(['filament.admin.auth.login', 'filament.admin.auth.register']) ? 'images/orbit-btc-large.png' : 'images/orbit-btc-header.png') }}"
         alt="Orbit BTC Logo"
         class="orbit-btc-logo"
     >
