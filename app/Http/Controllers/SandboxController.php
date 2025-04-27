03c6e660d0631fdb1dce29034ca1552a2aeaf027
Filipe Knoedt <fknoedt@gmail.com>
2025-04-27 00:05:13 -0400

    [trunk] sandbox update

--- app/Http/Controllers/SandboxController.php
+++ app/Http/Controllers/SandboxController.php
@@ -9,7 +9,7 @@ class SandboxController
     public function index(): void
     {
         if (config('app.env') !== 'local' || auth()->user()->role_id < 3) {
-            throw new AuthorizationException('Nope');
+            throw new AuthorizationException('Access denied and reported');
         }
 
         // playground -- you probably don't need to commit anything in this file
