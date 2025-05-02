3dcfbde94a2ad50cf259287c00af75da40745a46
Filipe Knoedt <fknoedt@gmail.com>
2025-05-01 23:04:02 -0400

    [trunk] adjusting schedule: every 1 became 30 minutes & shouldUpdateCryptoCompareStats fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,27 +31,27 @@ class Kernel extends ConsoleKernel
         $initialDate = Carbon::now('America/New_York')->subDays(self::PRICE_SYNC_LAST_X_DAYS)->format('Y-m-d');
 
         $schedule->command('btc:populate-price-history --since=' . $initialDate)
-            ->everyMinute()->when($this->shouldUpdatePrices($initialDate))
+            ->everyThirtyMinutes()->when($this->shouldUpdatePrices($initialDate))
             ->appendOutputTo($logPath)
             ->emailOutputOnFailure($emailErrorsTo);
 
         $schedule->command('btc:coin-market-cap-daily-stats-command')
-            ->everyMinute()->when($this->shouldUpdateCmcStats())
+            ->everyThirtyMinutes()->when($this->shouldUpdateCmcStats())
             ->appendOutputTo($logPath)
             ->emailOutputOnFailure($emailErrorsTo);
 
         $schedule->command('btc:mempool-daily-stats')
-            ->everyMinute()->when($this->shouldUpdateHashrate())
+            ->everyThirtyMinutes()->when($this->shouldUpdateHashrate())
             ->appendOutputTo($logPath)
             ->emailOutputOnFailure($emailErrorsTo);
 
         $schedule->command('btc:update-future-price-change')
-            ->everyMinute()->when($this->shouldUpdateFuturePriceChange())
+            ->everyThirtyMinutes()->when($this->shouldUpdateFuturePriceChange())
             ->appendOutputTo($logPath)
             ->emailOutputOnFailure($emailErrorsTo);
 
         $schedule->command('btc:update-mayer-multiple')
-            ->everyMinute()->when($this->shouldUpdateMayerMultiple())
+            ->everyThirtyMinutes()->when($this->shouldUpdateMayerMultiple())
             ->appendOutputTo($logPath)
             ->emailOutputOnFailure($emailErrorsTo);
 
@@ -60,7 +60,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(
             'btc:crypto-quant-daily-stats ' . implode(',', $cqMetrics) . ' --ignore-errors'
         )
-            ->everyMinute()->when($this->shouldUpdateCryptoQuantStats($cqMetrics))
+            ->everyThirtyMinutes()->when($this->shouldUpdateCryptoQuantStats($cqMetrics))
             ->appendOutputTo($logPath)
             ->emailOutputOnFailure($emailErrorsTo)
             ->onFailure(function (\Throwable $e) {
@@ -70,7 +70,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(
             'btc:cryptocompare-daily-stats --ignore-errors'
         )
-            ->everyMinute()->when($this->shouldUpdateCryptoCompareStats([
+            ->everyThirtyMinutes()->when($this->shouldUpdateCryptoCompareStats([
                 'transaction_count',
                 'large_transaction_count',
                 'average_transaction_value',
@@ -85,7 +85,7 @@ class Kernel extends ConsoleKernel
             });
 
         $schedule->command('btc:update-all-user-signal-scores')
-            ->everyMinute()->when($this->shouldUpdateUserSignals())
+            ->everyThirtyMinutes()->when($this->shouldUpdateUserSignals())
             ->appendOutputTo($logPath)
             ->emailOutputOnFailure($emailErrorsTo);
     }
@@ -131,7 +131,7 @@ class Kernel extends ConsoleKernel
     {
         $since = Carbon::now()->subDays(CryptoCompareDailyStatsCommand::FETCH_DAYS_AGO)->format('Y-m-d');
         $pricesMissingStats = DailyPrice::where('date', '>', $since)
-            ->where('date', '<', Carbon::now()->format('Y-m-d'))
+            ->where('date', '<=', Carbon::now()->format('Y-m-d'))
             ->where(function ($q) use ($columnsToUpdate) {
                 foreach ($columnsToUpdate as $column) {
                     $q->orWhereNull($column);
