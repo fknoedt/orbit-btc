83d826989d1f78e22811e788e2e5ce88a05d57d0
Filipe Knoedt <fknoedt@gmail.com>
2025-05-13 16:45:12 -0400

    [trunk] debugging adapter (Octante should be screwing it on prod)

--- app/Adapters/AdapterFactory.php
+++ app/Adapters/AdapterFactory.php
@@ -2,6 +2,8 @@
 
 namespace App\Adapters;
 
+use Illuminate\Support\Facades\Log;
+
 class AdapterFactory
 {
     public const string DEFAULT_ADAPTER = 'CoinGecko';
@@ -14,14 +16,19 @@ class AdapterFactory
      */
     public static function getAdapter(string $adapterName = null): ExternalApiAdapterInterface
     {
+        Log::info("adapter: {$adapterName}");
         $adapterName = $adapterName ?? self::DEFAULT_ADAPTER;
         $adapterName = ucfirst($adapterName);
 
+        Log::info("adapter: {$adapterName}");
+
         if (! isset(self::$adapters[$adapterName])) {
             $adapterClassName = '\App\Adapters\\' . $adapterName . 'ApiAdapter';
+            Log::info("adapterClassName: {$adapterClassName}");
 
             if (class_exists($adapterClassName)) {
                 $adapter = new $adapterClassName();
+                Log::info('instantiated adapter');
             }
 
             if (!isset($adapter) || !$adapter instanceof ExternalApiAdapterInterface) {
@@ -31,6 +38,9 @@ class AdapterFactory
             }
 
             self::$adapters[$adapterName] = $adapter;
+            Log::info("$adapterName saved with {$adapter->getClientName()}");
+        } else {
+            Log::info("adapter: {$adapterName} is already instantiated with " . self::$adapters[$adapterName]->getClientName());
         }
 
         return self::$adapters[$adapterName];
